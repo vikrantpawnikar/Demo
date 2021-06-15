97c90478044482a131911784d58414b930fda2fb
vikrantpawnikar <vikrant5488@gmail.com>
2021-06-15 13:23:46 +0100

    Update StringUtil.kt

--- StringUtil.kt
+++ StringUtil.kt
@@ -1,5 +1,9 @@
 package com.vikrant.screeningdemo
 
 class StringUtil {
-    fun filterInputString(input: String?) = input?.firstOrNull { c -> c in 'A'..'Z' } != null
+    fun filterInputString(input: String?) = input?.firstOrNull { c -> c in RANGE } != null
+
+    companion object {
+        private val RANGE = 'A'..'Z'
+    }
 }
